11e2ba232db1135e4a78e517c4c91e5b92959ee8
emmamidnic <email@example.com>
2022-05-23 11:21:38 +0200

    Oprettelse af BeregnMaxIltOptagelse metode

--- Praktikopgave 1/Program.cs
+++ Praktikopgave 1/Program.cs
@@ -14,11 +14,17 @@ namespace Praktikopgave_1
             Console.WriteLine("\n\nIndtast din maximale puls (slag pr. min.): ");
             double maxpuls = Convert.ToDouble(Console.ReadLine());
 
-            Kondital test = new Kondital(maxpuls, hvilepuls);
+            Console.WriteLine("\n\nIndtast din vægt i kg: ");
+            double vaegt = Convert.ToDouble(Console.ReadLine());
+            //Kondital test = new Kondital(maxpuls, hvilepuls);
 
-            string resultat = Kondital.BeregnKondital(maxpuls, hvilepuls);
+            double kondital = Kondital.BeregnKondital(maxpuls, hvilepuls);
 
-            Console.WriteLine(resultat);
+            Console.WriteLine("\n\nDit kondital er {0} ml/min pr. kg", kondital);
+
+            
+            double maxIltOptagelse = Kondital.BeregnMaxIltOptagelse(kondital, vaegt);
+            Console.WriteLine("\n\nDin maximale iltoptagelse er {0} l/ml", maxIltOptagelse);
 
             Console.ReadLine();
         }
@@ -32,22 +38,33 @@ namespace Praktikopgave_1
 
         private double maxpuls;
         private double hvilepuls;
+        private double vaegt;
 
 
-        public Kondital(double _maxpuls, double _hvilepuls)
+        public Kondital(double _maxpuls, double _hvilepuls, double _vaegt)
         {
             maxpuls = _maxpuls;
             hvilepuls = _hvilepuls;
-          
-
+            vaegt = _vaegt;
 
         }
 
-        public static string BeregnKondital(double maxpuls, double hvilepuls)
+        public static double BeregnKondital(double maxpuls, double hvilepuls)
         {
-            double kondital = Convert.ToInt32((maxpuls / hvilepuls) * 15.3);
+            double kondital = Convert.ToDouble((maxpuls / hvilepuls) * 15.3);
 
-            return string.Format("\n\nDit kondital er {0} ml/min pr. kg", kondital);
+            return kondital;
+        }
+
+        public static double BeregnMaxIltOptagelse(double kondital, double vaegt)
+        {
+
+            double maxIltOptagelse = Math.Round((kondital * vaegt) / 1000, 1);
+            return maxIltOptagelse;
+
+            //Maximal iltoptagelse, afrundet til 1 decimal og med benævnelsen l / ml HJÁLP FRÁ ANDY !
+
+
         }
     }
 }
