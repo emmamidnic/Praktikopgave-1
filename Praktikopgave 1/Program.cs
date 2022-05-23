3f79f9df4da80a89a87cd1ee093bc983ba74cf17
emmamidnic <email@example.com>
2022-05-23 10:53:50 +0200

    Oprettelse af BeregnKondital metode

--- Praktikopgave 1/Program.cs
+++ Praktikopgave 1/Program.cs
@@ -6,18 +6,48 @@ namespace Praktikopgave_1
     {
         static void Main(string[] args)
         {
-            
-            
+
+
             Console.WriteLine("Indtast din hvilepuls (slag pr. min.): ");
             double hvilepuls = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("\n\nIndtast din maximale puls (slag pr. min.): ");
-            double maxPuls = Convert.ToDouble(Console.ReadLine());
+            double maxpuls = Convert.ToDouble(Console.ReadLine());
+
+            Kondital test = new Kondital(maxpuls, hvilepuls);
+
+            string resultat = Kondital.BeregnKondital(maxpuls, hvilepuls);
+
+            Console.WriteLine(resultat);
+
+            Console.ReadLine();
         }
+
+
+
     }
 
     class Kondital
     {
 
+        private double maxpuls;
+        private double hvilepuls;
+
+
+        public Kondital(double _maxpuls, double _hvilepuls)
+        {
+            maxpuls = _maxpuls;
+            hvilepuls = _hvilepuls;
+          
+
+
+        }
+
+        public static string BeregnKondital(double maxpuls, double hvilepuls)
+        {
+            double kondital = Convert.ToInt32((maxpuls / hvilepuls) * 15.3);
+
+            return string.Format("\n\nDit kondital er {0} ml/min pr. kg", kondital);
+        }
     }
 }
